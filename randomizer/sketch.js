5ee80dc940e4e42a242b41ab3645c27c509384d6
sjsu-koby <koby.nguyen@sjsu.edu>
2022-03-07 01:24:33 -0800

    added folder and edited sketch

--- randomizer/sketch.js
+++ randomizer/sketch.js
@@ -1,14 +1,12 @@
-var personas = ['koby']
+let persona = ["koby"];
 
 function setup() {
-  create canvas(600,600);
+  createCanvas(600, 600);
   background(200);
-
   console.log(persona[0]);
 
 }
 
 function draw() {
-
 
 }
