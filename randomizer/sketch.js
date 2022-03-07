1fae106854c624ab4ec1b5db05e4aa67ecbd904a
sjsu-koby <koby.nguyen@sjsu.edu>
2022-03-07 01:54:05 -0800

    added console logs

--- randomizer/sketch.js
+++ randomizer/sketch.js
@@ -1,10 +1,19 @@
-var persona = ["koby"]
+var personas = ["Koby1","Koby2","Koby3","Koby4"]
 
 function setup() {
   createCanvas(600, 600);
   background(220);
 
-  console.log(persona[0]);
+  console.log("initial array is")
+  console.log(personas);
+
+  personas.shift();
+  console.log("array after shift");
+  console.log(personas);
+
+  personas.unshift("Koby5");
+  console.log("array after unshift")
+  console.log(personas);
 }
 
 function draw() {
