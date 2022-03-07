951178258d0946e6e84ca04dc69aac41cd61b8a6
sjsu-koby <koby.nguyen@sjsu.edu>
2022-03-07 01:43:32 -0800

    remade entire randomizer

--- randomizer/sketch.js
+++ randomizer/sketch.js
@@ -1,10 +1,10 @@
-let persona = ["koby"];
+var persona = ["koby"]
 
 function setup() {
   createCanvas(600, 600);
-  background(200);
-  console.log(persona[0]);
+  background(220);
 
+  console.log(persona[0]);
 }
 
 function draw() {
